97643f6c300c60e3745f5b96b07dd6f0fe3bb998
milltoy <pornratmill@gmail.com>
2024-07-22 17:04:33 -0400

    update model

--- location-service/src/main/java/com/chickpic/microservices/location/model/City.java
+++ location-service/src/main/java/com/chickpic/microservices/location/model/City.java
@@ -4,19 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Unwrapped;
 
-import java.util.List;
-
-@Document(value="location")
+@Document(value="city")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Data
-public class Location {
-    @Id
-    private String country;
-    private List<City> cities;
+public class City {
+    private String city;
+    private double lat;
+    private double lng;
 }
